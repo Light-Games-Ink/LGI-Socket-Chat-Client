a53fbf4dff7acc5447622e31fe14fb14a4e5aff0
LaughingMaan <laughingmaan@icloud.com>
2015-12-11 00:36:14 +0300

    Initial commit

---
+++ LGI-Socket-Chat-Client/src/ru/lgi/main/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ru.lgi.main;
+
+/**
+ * @author LaughingMaan
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
