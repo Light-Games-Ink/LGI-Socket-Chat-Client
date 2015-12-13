dd2daa7c4f38dc65ce9653ba4e4081362f090763
LaughingMaan <laughingmaan@icloud.com>
2015-12-14 02:14:01 +0300

    Done with basic network. Code cleanup.

--- LGI-Socket-Chat-Client/src/ru/lgi/main/Main.java
+++ LGI-Socket-Chat-Client/src/ru/lgi/main/Main.java
@@ -6,16 +6,15 @@ package ru.lgi.main;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.SocketAddress;
-import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.charset.Charset;
 
 import javax.swing.JOptionPane;
 
-import naga.NIOService;
 import naga.NIOSocket;
-import naga.SocketObserverAdapter;
+import naga.SocketObserver;
+import naga.eventmachine.EventMachine;
+import naga.packetreader.AsciiLinePacketReader;
+import naga.packetwriter.AsciiLinePacketWriter;
 
 /**
  * @author LaughingMaan
@@ -23,13 +22,11 @@ import naga.SocketObserverAdapter;
  */
 public class Main {
 	static MainWindow window;
-	static String msg;
-	//private static Attachment attach;
-	//private static ReadWriteHandler readWriteHandler;
-	private static Charset cs = Charset.forName("UTF-8");;
-	//private static AsynchronousSocketChannel channel;
+	private static String msg;
+	private static Charset cs = Charset.forName("UTF-8");
 	private static int port;
 	private static NIOSocket socket;
+	static byte[] content;
 
 	/**
 	 * @param args
@@ -38,39 +35,51 @@ public class Main {
 
 		window = new MainWindow();
 		window.setVisible(true);
-		try { 
-			port = 5676;
-			NIOService service = new NIOService();
-			socket = service.openSocket("localhost", port);
-			socket.isOpen();
-			if(socket.socket().isConnected()){window.statusLabel.setText("Connected");}
-			else{window.statusLabel.setText("Unable to connect");}
-			socket.listen(new SocketObserverAdapter(){
-				@Override 
-				public void packetReceived(NIOSocket socket, byte[] packet) {
-					//Check the packet, then made a broadcast
-					msg = new String(packet,cs);
-					window.chatTextEditor.setText(window.chatTextEditor.getText() + msg + " End of message");
-					
-					
-			}});
-			
+		try {
 
-			/*channel = AsynchronousSocketChannel.open();
-			SocketAddress serverAddr = new InetSocketAddress("localhost", 5678);
-			Future<Void> result = channel.connect(serverAddr);
-			result.get();
-			window.statusLabel.setText("Connected"); // change to label
-			attach = new Attachment();
-			attach.channel = channel;
-			attach.buffer = ByteBuffer.allocate(2048);
-			attach.isRead = false;
-			attach.mainThread = Thread.currentThread();*/
+			port = 5674;
+			EventMachine machine = new EventMachine();
+			socket = machine.getNIOService().openSocket("localhost", port);
+			socket.setPacketReader(new AsciiLinePacketReader());
+			socket.setPacketWriter(new AsciiLinePacketWriter());
+			machine.start();
+			socket.listen(new SocketObserver() {
+
+				@Override
+				public void packetSent(NIOSocket socket, Object tag) {
+
+				}
+
+				@Override
+				public void packetReceived(NIOSocket socket, byte[] packet) {
+					// new message!!
+					try {
+						msg = new String(packet, cs);
+						window.chatTextEditor.setText(window.chatTextEditor.getText() + msg); // check
+																								// it
+
+					} catch (Exception ex) {
+						JOptionPane.showMessageDialog(null, ex.getMessage());
+					}
+				}
+
+				@Override
+				public void connectionOpened(NIOSocket nioSocket) {
+					window.statusLabel.setText("Connected");
+
+				}
+
+				@Override
+				public void connectionBroken(NIOSocket nioSocket, Exception exception) {
+					JOptionPane.showMessageDialog(null, "Connection broken");
+
+				}
+			});
 
 		} catch (IOException e) {
 			e.printStackTrace();
 			JOptionPane.showMessageDialog(null, e.toString());
-		} 
+		}
 		window.userTextArea.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyReleased(KeyEvent e) {
@@ -81,38 +90,19 @@ public class Main {
 			public void keyReleased(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_ENTER && !e.isControlDown()) {
 					if (!window.userTextArea.getText().equals("")) {
-						msg = window.userTextArea.getText();
-						//window.chatTextArea.setText(window.chatTextArea.getText() + msg);
-						
-						if(!socket.write(msg.getBytes())){
-							JOptionPane.showMessageDialog(null, "Queque error!");
-						}
+						socket.write(window.userTextArea.getText()
+								.substring(0, window.userTextArea.getText().length() - 1).getBytes(cs));
+
 						window.userTextArea.setText("");
+
 					} else {
 						window.userTextArea.setText("");
 					}
 
-					// window.usersList.setText(window.userTextArea.getText());
 				}
 			}
 		});
-		
-	}
 
-	/*private static void sendMsg(String msg) {
-		cs = Charset.forName("UTF-8");
-		msg = window.chatTextArea.getText();
-		window.userTextArea.setText("");
-		byte[] data = msg.getBytes(cs);
-		attach.buffer.put(data);
-		attach.buffer.flip();
-		readWriteHandler = new ReadWriteHandler();
-		channel.write(attach.buffer, attach, readWriteHandler);
-		try {
-			attach.mainThread.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}*/
+	}
 
 }
